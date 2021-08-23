4d9bc3220f4a7399d40c3e9ffbd849dd6cc2c273
tayebehsafdari <66350369+tayebehsafdari@users.noreply.github.com>
2021-08-23 18:23:58 +0430

    modify files

--- src/components/Link.test.js
+++ src/components/Link.test.js
@@ -2,9 +2,15 @@
 
 import React from "react";
 import {create, act} from 'react-test-renderer';
+import ShallowRenderer from 'react-test-renderer/shallow';
 import Link from "./Link";
 
 test('Link changes the class when hovered', () => {
+    const renderer = new ShallowRenderer();
+    renderer.render(<Link page="http://www.facebook.com">Facebook</Link>);
+    const result = renderer.getRenderOutput();
+    console.log("ShallowRenderer: ", result);
+
     let testRenderer;
     act(() => {
         testRenderer = create(
